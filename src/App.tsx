36d4509f70c580ce210a7d2c5279521ff7fde7e9
lucasbentu <lucas.bento38@gmail.com>
2024-01-24 11:52:17 -0300

    feat: add styled components and create button to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </div>
   )
 }
